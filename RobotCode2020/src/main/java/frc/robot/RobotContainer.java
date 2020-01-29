5466e148da4cab5b8482858be9f2366e8380da2c
Aaron <abeck0835@gmail.com>
2020-01-28 20:23:34 -0600

    Updated and deleted spare files

--- RobotCode2020/src/main/java/frc/robot/RobotContainer.java
+++ RobotCode2020/src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,6 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
